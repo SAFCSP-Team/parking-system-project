5b7988dd04ac6d0904a0e3c5bd13ba14907d8518
JSQdev <148073798+devJSQ@users.noreply.github.com>
2025-07-15 04:30:58 +0300

    Update Employee.java

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,19 @@
-public class Employee {
-  
+import java.util.*;
+
+class Employee {
+    String name;
+    int id;
+    String vehicle;
+
+    public Employee(String name, int id, String vehicle) {
+        this.name = name;
+        this.id = id;
+        this.vehicle = vehicle;
+    }
+
+    public String toString() {
+        return "name: " + name + ", ID: " + id + ", vehicle: " + vehicle;
+    }
 }
+
+
